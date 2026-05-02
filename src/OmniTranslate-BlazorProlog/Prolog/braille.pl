6b6cd2e19c9912974eeaaa3f0889c3f3747e3971
goncalo.amador <goncalo.amador@symbioticsgroup.com>
2026-05-02 13:00:55 +0100

    Prolog final pl files

---
+++ src/OmniTranslate-BlazorProlog/Prolog/braille.pl
@@ -0,0 +1,40 @@
+% BRAILLE -> ENGLISH DICTIONARY
+
+braille("a", "100000").
+braille("b", "110000").
+braille("c", "100100").
+braille("d", "100110").
+braille("e", "100010").
+braille("f", "110100").
+braille("g", "110110").
+braille("h", "110010").
+braille("i", "010100").
+braille("j", "010110").
+braille("k", "101000").
+braille("l", "111000").
+braille("m", "101100").
+braille("n", "101110").
+braille("o", "101010").
+braille("p", "111100").
+braille("q", "111110").
+braille("r", "111010").
+braille("s", "011100").
+braille("t", "011110").
+braille("u", "101001").
+braille("v", "111001").
+braille("w", "010111").
+braille("x", "101101").
+braille("y", "101111").
+braille("z", "101011").
+
+braille("#", "001111").
+braille("1", "100000").
+braille("2", "110000").
+braille("3", "100100").
+braille("4", "100110").
+braille("5", "100010").
+braille("6", "110100").
+braille("7", "110110").
+braille("8", "110010").
+braille("9", "010100").
+braille("0", "010110").
---
+++ src/OmniTranslate-BlazorProlog/Prolog/orc.pl
@@ -0,0 +1,20 @@
+% ORC -> ENGLISH DICTIONARY
+
+orc_word("hi", "charach").
+orc_word("bye", "futchi").
+orc_word("yes", "mok").
+orc_word("no", "nok").
+orc_word("buy", "goshak").
+orc_word("weapon", "porack").
+orc_word("sword", "burkabata").
+orc_word("sabre", "charcha").
+orc_word("hatchet", "hakhak").
+orc_word("shortsword", "burka").
+orc_word("bow", "batuk").
+orc_word("arrows", "pixo").
+orc_word("armor", "bora").
+orc_word("shield", "dora").
+orc_word("brassshield", "donga").
+orc_word("studded", "tulak").
+orc_word("leather", "aka").
+orc_word("studdedhelmet", "grofa").
